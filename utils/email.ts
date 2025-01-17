9bb6cefd5e26d1a252c0f95b8ed9e5dac7a75832
StefanosTheDev <stefanosdev26@gmail.com>
2025-01-17 15:25:23 -0800

    Email Endpoint Is working successfully

--- utils/email.ts
+++ utils/email.ts
@@ -3,16 +3,17 @@ import AppError from '../error/AppError';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const apiKey = process.env.SENDGRID_API_KEY as string;
-if (!apiKey) {
-  throw new AppError(
-    'SendGrid API key is not set. Check your environment variables',
-    404
-  );
-}
-sgMail.setApiKey(apiKey);
 
 export async function sendEmail(to: string, subject: string, message: string) {
+  const apiKey = process.env.SENDGRID_API_KEY as string;
+  if (!apiKey) {
+    throw new AppError(
+      'SendGrid API key is not set. Check your environment variables',
+      404
+    );
+  }
+  sgMail.setApiKey(apiKey);
+
   // Define the email message
   const msg: sgMail.MailDataRequired = {
     to: to, // Replace with recipient's email
@@ -21,13 +22,8 @@ export async function sendEmail(to: string, subject: string, message: string) {
     text: message, // Plain text message
     html: `<strong>${message}</strong>`, // HTML message including the variable
   };
-  const delay = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
 
   try {
-    console.log('Waiting for 5 seconds before sending the email...');
-    await delay(5000); // Wait for 5 seconds
-
     await sgMail.send(msg);
     console.log('Email sent successfully!');
   } catch (error) {
