876c2768ad38bef5d93367ba1e012465900f1ae2
StefanosTheDev <stefanosdev26@gmail.com>
2025-01-17 13:42:39 -0800

    SendGrid API Key Synced

--- grid.ts
+++ grid.ts
@@ -1,17 +1,17 @@
 import sgMail from '@sendgrid/mail';
 
-// Set the API key
-sgMail.setApiKey(
-  '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'
-);
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 // Define the email message
 const msg: sgMail.MailDataRequired = {
   to: 'stefanos@clearstack.ai', // Replace with recipient's email
   from: 'no-reply@em9716.clearstack.ai', // Replace with your verified SendGrid sender email
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>Stefanos Sophocleous is here</strong>',
+  html: '<strong>Stefanos Sophocleous Most Updated Email</strong>',
 };
 
 // Send the email
